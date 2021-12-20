afdd419d82e0d6f791bb22f11b5db42d7d1c844b
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2021-12-20 10:54:22 -0700

    api port updates

--- api/app.js
+++ api/app.js
@@ -112,9 +112,9 @@ app.use(function (err, req, res, next) {
 
 //server
 
-const port = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Server active on port ${port}`);
 });
 
